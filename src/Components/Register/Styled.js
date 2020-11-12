4c400a3a4b3b4503f05cbc49d2fb5c2aea27feb3
pdlma0904 <57673815+pdlma0904@users.noreply.github.com>
2020-11-12 17:26:13 +0900

    [UPDATE] Css
    login and register

--- src/Components/Register/Styled.js
+++ src/Components/Register/Styled.js
@@ -1,30 +1,34 @@
 import styled from "styled-components";
 
 const SelectFormTextWrapper = styled.div`
-  flex: 0.8;
-  align-self: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20%;
   span {
     font-size: 28px;
   }
 `;
 
 const SelectFormContainer = styled.div`
-  flex: 0.5;
-  align-self: center;
+  width: 70%;
+  height: 80%;
   display: flex;
-  flex-direction: row;
-  padding-bottom: 2vh;
+  justify-content: space-around;
+  align-items:center;
 `;
 
 const SelectItem = styled.div`
-  margin: 2vw 1vw 2vw 1vw;
   display: flex;
-  flex-direction: column;
+  justify-content: space-around;
   label {
+    width: 250px;
+    height: 250px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     border: 3px solid gray;
     border-radius: 1vw;
-    padding: 8vw 8vw 8vw 8vw;
-    flex: 2;
     &.teacher {
       background-color: #066c9a;
       color: white;
