d627f1ac521fff72942392d0e2a638accf649788
유영재 <57673815+pdlma0904@users.noreply.github.com>
2021-02-05 20:58:45 +0900

    Create Teacher.js

---
+++ src/Pages/Teacher.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Footer, Header, Teacher } from "../Components/index";
+
+const TeacherPage = () => {
+  return (
+    <>
+      <Header />
+      <Teacher />
+      <Footer />
+    </>
+  );
+};
+
+export default TeacherPage;
